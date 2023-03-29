c2bb9b36be908dc1ac4a7c3bb91fd581469456f4
rdush <rdush824@gmail.com>
2023-03-29 17:27:05 +0300

    add routes and contrs

---
+++ server/controllers/auth.js
@@ -0,0 +1,28 @@
+import User from '../models/User.js';
+
+// Register user
+export const register = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//Login user
+export const login = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//Get me
+export const getMe = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from 'cors';
 
+import authRoute from './routes/auth.js'
+
 const app = express();
 dotenv.config();
 
@@ -15,6 +17,8 @@ const DB_NAME = process.env.DB_NAME;
 // Middleweare
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/auth', authRoute)
 
 async function start() {
     try {
