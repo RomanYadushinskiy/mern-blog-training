eec2edcc7d01484ff1c0c1991b9c1dd3b8ab8945
rdush <rdush824@gmail.com>
2023-03-30 00:11:44 +0300

    end backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { register, login, getMe } from "../controllers/auth.js";
-
+import { checkAuth } from "../utils/checkAuth.js";
 
 const router = new Router();
 
@@ -10,10 +10,10 @@ router.post('/register', register)
 
 // Login
 // http://localhost:3002/api/auth/login
-router.post('/lohin', login)
+router.post('/login', login)
 
 // Get Me
 // http://localhost:3002/api/auth/me
-router.post('/me', getMe)
+router.post('/me', checkAuth, getMe)
 
 export default router;
