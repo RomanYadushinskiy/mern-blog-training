3437a0e1141b7b7cfa4ff80a34d64f4f13920950
rdush <rdush824@gmail.com>
2023-03-30 23:22:52 +0300

    add Navbar

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar } from "./Navbar";
+
+export const Layout = ({children}) => {
+    return <React.Fragment>
+        <div className="container ax-auto">
+            <Navbar />
+            {children}
+    </div>
+    </React.Fragment>
+    
+}
+
+
+
+
